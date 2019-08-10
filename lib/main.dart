186e2fec293a6366ce85a7a54f4eef44f1bf727f
Martin Stofko <martinstofko114@gmail.com>
2019-08-09 23:10:29 -0700

    Starting the new course app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,9 +19,95 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '1',
+      description: 'New Shoes',
+      amount: 149.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      description: 'Groceries',
+      amount: 84.65,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spending Tracker'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            child: Text('chart'),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 16.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Description'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: transactions.map((t) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 8.0,
+                        horizontal: 16.0,
+                      ),
+                      child: Text(
+                        '\$${t.amount}',
+                        style: Theme.of(context).textTheme.subhead.apply(
+                              color: Colors.red,
+                              fontWeightDelta: 2,
+                            ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          t.description,
+                          style: Theme.of(context).textTheme.subtitle,
+                        ),
+                        Text(
+                          DateFormat.yMMMMEEEEd().add_jm().format(t.date),
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
