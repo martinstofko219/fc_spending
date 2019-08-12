3c51fbc8c00704457b3e9bc00b35d4297fa13900
Martin Stofko <martinstofko114@gmail.com>
2019-08-11 20:14:38 -0700

    Added no transaction state.

--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -12,51 +12,69 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450.0,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          final t = transactions[index];
-
-          return Card(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: (transactions.isEmpty)
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 8.0,
-                    top: 16.0,
-                    bottom: 16.0,
+                Container(
+                  height: 300.0,
+                  margin: EdgeInsets.only(bottom: 16.0),
+                  child: Image.asset(
+                    'assets/images/no-records.png',
+                    fit: BoxFit.cover,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                Text(
+                  'No transactions',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                final t = transactions[index];
+
+                return Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        t.description,
-                        style: Theme.of(context).textTheme.subtitle,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 8.0,
+                          top: 16.0,
+                          bottom: 16.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              t.description,
+                              style: Theme.of(context).textTheme.subtitle,
+                            ),
+                            Text(
+                              DateFormat.yMMMMEEEEd().add_jm().format(t.date),
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ],
+                        ),
                       ),
-                      Text(
-                        DateFormat.yMMMMEEEEd().add_jm().format(t.date),
-                        style: Theme.of(context).textTheme.caption,
+                      Container(
+                        padding: EdgeInsets.only(right: 8.0),
+                        child: Text(
+                          '\$${t.amount.toStringAsFixed(2)}',
+                          textAlign: TextAlign.right,
+                          style: Theme.of(context).textTheme.subhead.copyWith(
+                                color: Colors.red,
+                                fontWeight: FontWeight.w500,
+                              ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(right: 8.0),
-                  child: Text(
-                    '\$${t.amount.toStringAsFixed(2)}',
-                    textAlign: TextAlign.right,
-                    style: Theme.of(context).textTheme.subhead.copyWith(
-                          color: Colors.red,
-                          fontWeight: FontWeight.w500,
-                        ),
-                  ),
-                ),
-              ],
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
