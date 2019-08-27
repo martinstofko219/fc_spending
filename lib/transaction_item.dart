795d95cd46bd98e02e7e3a8c13e1d20b18a59784
Martin Stofko <martinstofko114@gmail.com>
2019-08-26 19:39:07 -0700

    Refactoring

---
+++ lib/transaction_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'models/transaction.dart';
+
+class TransactionItem extends StatelessWidget {
+  final Transaction transaction;
+  final void Function(String) onDeleteTransaction;
+
+  const TransactionItem({
+    Key key,
+    @required this.transaction,
+    @required this.onDeleteTransaction,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Dismissible(
+        key: Key(transaction.id),
+        background: Container(
+          alignment: Alignment.centerRight,
+          color: Theme.of(context).errorColor,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: const Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          if (direction != DismissDirection.endToStart) {
+            return;
+          }
+          onDeleteTransaction(transaction.id);
+        },
+        child: ListTile(
+          trailing: Text(
+            '\$${transaction.amount.toStringAsFixed(2)}',
+            textAlign: TextAlign.right,
+            style: Theme.of(context).textTheme.body1.copyWith(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w500,
+                ),
+          ),
+          title: Text(
+            transaction.description,
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          subtitle:
+              Text(DateFormat.yMMMMEEEEd().add_jm().format(transaction.date)),
+        ),
+      ),
+    );
+  }
+}
--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'models/transaction.dart';
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,48 +35,11 @@ class TransactionList extends StatelessWidget {
           )
         : ListView.builder(
             itemCount: transactions.length,
-            itemBuilder: (ctx, index) {
+            itemBuilder: (context, index) {
               final t = transactions[index];
 
-              return Card(
-                child: Dismissible(
-                  key: Key(t.id),
-                  background: Container(
-                    alignment: Alignment.centerRight,
-                    color: Theme.of(context).errorColor,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 16.0),
-                      child: const Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  direction: DismissDirection.endToStart,
-                  onDismissed: (direction) {
-                    if (direction != DismissDirection.endToStart) {
-                      return;
-                    }
-                    onDeleteTransaction(t.id);
-                  },
-                  child: ListTile(
-                    trailing: Text(
-                      '\$${t.amount.toStringAsFixed(2)}',
-                      textAlign: TextAlign.right,
-                      style: Theme.of(context).textTheme.body1.copyWith(
-                            color: Colors.red,
-                            fontWeight: FontWeight.w500,
-                          ),
-                    ),
-                    title: Text(
-                      t.description,
-                      style: TextStyle(fontWeight: FontWeight.w500),
-                    ),
-                    subtitle:
-                        Text(DateFormat.yMMMMEEEEd().add_jm().format(t.date)),
-                  ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: t, onDeleteTransaction: onDeleteTransaction);
             },
           );
   }
